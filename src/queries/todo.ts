c8e3d354ac996538b2ac8f9dd2d36147ebbe35de
ClintonTSR <soonren20@icloud.com>
2025-02-05 01:20:38 +0800

    fix: typo mutation params

--- src/queries/todo.ts
+++ src/queries/todo.ts
@@ -1,16 +1,14 @@
-import { useInfiniteQuery, useMutation, useQuery } from '@tanstack/react-query'
-import { CreateTodoRequest, Todo, UpdateTodoRequest } from './todo.type'
-import { PaginatedResponse } from '../lib/cursor'
+import { useInfiniteQuery, useMutation, useQuery } from "@tanstack/react-query";
+import { CreateTodoRequest, Todo, UpdateTodoRequest } from "./todo.type";
+import { PaginatedResponse } from "../lib/cursor";
 
 export const useGetTodoByIdQuery = (id: string) => {
     return useQuery({
         queryKey: ['todo', id],
         queryFn: async () => {
-            const response = await fetch(
-                `${import.meta.env.VITE_API_URL}/api/todo/${id}`
-            )
-            return await response.json()
-        },
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/todo/${id}`);
+            return await response.json();
+        }
     })
 }
 
@@ -18,74 +16,62 @@ export const useGetTodosQuery = (queries: { limit: number }) => {
     return useInfiniteQuery<PaginatedResponse<Todo>>({
         queryKey: ['todos', queries],
         queryFn: async ({ pageParam }) => {
-            const params = new URLSearchParams()
-            params.append('limit', String(queries.limit))
+            const params = new URLSearchParams();
+            params.append('limit', String(queries.limit));
 
             if (pageParam) {
-                params.append('afterCursor', pageParam as string)
+                params.append('afterCursor', pageParam as string);
             }
 
-            const response = await fetch(
-                `${import.meta.env.VITE_API_URL}/api/todo?${params.toString()}`
-            )
-            return await response.json()
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/todo?${params.toString()}`);
+            return await response.json();
         },
         getNextPageParam: (data) => {
-            return data.cursor.afterCursor
+            return data.cursor.afterCursor;
         },
         getPreviousPageParam: (data) => {
-            return data.cursor.beforeCursor
+            return data.cursor.beforeCursor;
         },
         initialPageParam: null,
     })
 }
 
-export const useCreateTodoMutation = (todo: CreateTodoRequest) => {
+export const useCreateTodoMutation = () => {
     return useMutation({
-        mutationFn: async () => {
-            const response = await fetch(
-                `${import.meta.env.VITE_API_URL}/api/todo`,
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(todo),
-                }
-            )
-            return response.json()
-        },
+        mutationFn: async (todo: CreateTodoRequest) => {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/todo`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(todo)
+            });
+            return response.json();
+        }
     })
 }
 
-export const useUpdateTodoMutation = (todo: UpdateTodoRequest) => {
+export const useUpdateTodoMutation = () => {
     return useMutation({
-        mutationFn: async () => {
-            const response = await fetch(
-                `${import.meta.env.VITE_API_URL}/api/todo/${todo.id}`,
-                {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(todo),
-                }
-            )
-            return response.json()
-        },
+        mutationFn: async (todo: UpdateTodoRequest) => {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/todo/${todo.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(todo)
+            });
+            return response.json();
+        }
     })
 }
 
-export const useDeleteTodoMutation = (id: string) => {
-    return useMutation({
-        mutationFn: async () => {
-            const response = await fetch(
-                `${import.meta.env.VITE_API_URL}/api/todo/${id}`,
-                {
-                    method: 'DELETE',
-                }
-            )
-            return response.json()
-        },
-    })
+export const useDeleteTodoMutation = (id: string) => { return useMutation({
+    mutationFn: async () => {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/todo/${id}`, {
+            method: 'DELETE',
+    });
+        return response.json();
+    }
+})
 }
