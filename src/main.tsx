699f4f3bebc3254a38e53b482fe99b453ebaa1a1
ClintonTSR <soonren20@icloud.com>
2025-02-05 00:59:27 +0800

    fix: remove extra margin

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 // import LoginPage from "./pages/LoginPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import TodoListPage from "./pages/TodoListPage";
-import { Container } from "@mui/material";
 // import TodoPage from "./pages/TodoPage";
 
 const router = createBrowserRouter([
@@ -23,9 +22,7 @@ export const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <Container>
                 <RouterProvider router={router}/>
-            </Container>
         </QueryClientProvider>
     </React.StrictMode>,
 )
